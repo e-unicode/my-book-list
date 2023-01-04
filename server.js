4b07077e350beac3a7aac5e69b5c916cb272d66d
e-unicode <yangzzanga@gmail.com>
2023-01-04 19:09:46 +0900

    글번호 달기

--- server.js
+++ server.js
@@ -1,62 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.urlencoded({extended: true}));
-const MongoClient = require('mongodb').MongoClient;
-app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({ extended: true }));
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
 
 var db;
-MongoClient.connect('mongodb+srv://rlool:Eun2Eun2@simple-board-cluster.vtup2xr.mongodb.net/?retryWrites=true&w=majority', function(에러, client) {
-    if (에러) return console.log(에러)
-    db = client.db('todoapp');
 
-// 서버코드짤때 쓰는 공식: 뭔갈 저장하고싶을때, Object자료형으로 저장
-// db.collection('post').insertOne({이름 : 'EUNI', _id : 100, 나이 : 20}, function(에러, 결과) {
-//     console.log('저장완료');
-// });
-    
-    
-    app.listen(8080, function() {
-        console.log('listening on 8080')
+MongoClient.connect(
+  "mongodb+srv://rlool:Eun2Eun2@simple-board-cluster.vtup2xr.mongodb.net/?retryWrites=true&w=majority",
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    db = client.db("todoapp");
+
+    // db.collection("post").insertOne(
+    //   { 이름: "EUNI", _id: 100, 나이: 20 },
+    //   function (에러, 결과) {
+    //     console.log("저장완료");
+    //   }
+    // );
+
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+  }
+);
+
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", function (요청, 응답) {
+  응답.send("전송완료");
+  db.collection("counter").findOne(
+    { name: "postCount" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+      db.collection("post").insertOne( 
+        {
+          _id: 총게시물갯수 + 1,
+          title: 요청.body.title,
+          artist: 요청.body.artist,
+        },
+        function (에러, 결과) {
+          console.log("저장완료");
+          db.collection("counter").updateOne(
+            { name: "postCount" },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+app.get("/list", function (요청, 응답) {
+  db.collection("post")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      응답.render("list.ejs", { posts: 결과 });
     });
 });
 
-app.get('/', function(요청, 응답) {
-    응답.sendFile(__dirname + '/index.html');
-});
-
-app.get('/write', function(요청, 응답) {
-    응답.sendFile(__dirname + '/write.html');
-});
-
-app.post('/add', function(요청, 응답) {
-    응답.send('전송완료');
-    db.collection('counter').findOne({ name : "postCount" }, function(에러, 결과) {
-        console.log(결과.totalPost);
-        var 총게시물갯수 = 결과.totalPost;
-        db.collection('post').insertOne({ _id : 총게시물갯수 + 1, title : 요청.body.title, artist : 요청.body.artist }, function(에러, 결과) {
-            console.log('저장완료');
-            db.collection('counter').updateOne({ name : "postCount" }, { $inc : {totalPost : 1} }, function(에러, 결과){
-                if(에러){return console.log(에러)}
-            });
-        });
-    });
-});
-
-
-
-app.get('/list', function(요청, 응답) {
-    // 데이터를 찾을때 쓰는 공식
-    db.collection('post').find().toArray(function(에러, 결과) {
-        console.log(결과);
-        // Object 형태로 결과 들어옴
-        응답.render('list.ejs', {posts : 결과});
-    });
-});
-
-
-
-app.delete('/delete', function(요청, 응답){
-    console.log(요청.body);
-    
+app.delete("/delete", function (요청, 응답) {
+  console.log(요청.body);
 });
