7f12ec1b8e496ad7e24fe88ede0ba68a2abf8a18
e-unicode <yangzzanga@gmail.com>
2023-01-05 20:07:00 +0900

    ejs파일로 수정

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
+app.use('/public', express.static('public'));
 
 var db;
 
@@ -26,11 +27,11 @@ MongoClient.connect(
 );
 
 app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+  응답.render("index.ejs");
 });
 
 app.get("/write", function (요청, 응답) {
-  응답.sendFile(__dirname + "/write.html");
+  응답.render("write.ejs");
 });
 
 app.post("/add", function (요청, 응답) {
@@ -40,7 +41,7 @@ app.post("/add", function (요청, 응답) {
     function (에러, 결과) {
       console.log(결과.totalPost);
       var 총게시물갯수 = 결과.totalPost;
-      db.collection("post").insertOne( 
+      db.collection("post").insertOne(
         {
           _id: 총게시물갯수 + 1,
           title: 요청.body.title,
@@ -74,4 +75,17 @@ app.get("/list", function (요청, 응답) {
 
 app.delete("/delete", function (요청, 응답) {
   console.log(요청.body);
+  요청.body._id = parseInt(요청.body._id);
+  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+    console.log("삭제완료");
+    응답.status(200).send({ message: "success" });
+  });
+});
+
+app.get("/detail/:id", function (요청, 응답) {
+  요청.params.id = parseInt(요청.params.id);
+  db.collection("post").findOne({ _id: 요청.params.id }, function (에러, 결과) {
+    console.log(결과);
+    응답.render("detail.ejs", { data: 결과 });
+  });
 });
