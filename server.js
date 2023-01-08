d0803928102bae3b5bf0d54871b4f4dc6117a5ab
e-unicode <yangzzanga@gmail.com>
2023-01-09 01:51:09 +0900

    복구

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
 app.use(express.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+const { ObjectId } = require("mongodb");
 
 var db;
 
@@ -13,7 +19,7 @@ MongoClient.connect("mongodb+srv://rlool:Eun2Eun2@simple-board-cluster.vtup2xr.m
   if (에러) return console.log(에러);
   db = client.db("todoapp");
 
-  app.listen(8080, function () {
+  http.listen(8080, function () {
     console.log("listening on 8080");
   });
 });
@@ -205,4 +211,100 @@ app.post("/member", function (요청, 응답) {
     console.log("아이디를 4자이상 10자 이하로 넣어주세요.");
     응답.redirect("/member");
   }
+});
+
+app.use("/shop", require("./routes/shop.js"));
+app.use("/board/sub", require("./routes/board.js"));
+
+let multer = require("multer");
+var storage = multer.diskStorage({
+  destination: function (req, file, cd) {
+    cd(null, "./public/image");
+  },
+  filename: function (req, file, cd) {
+    cd(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.get("/upload", function (요청, 응답) {
+  응답.render("upload.ejs");
+});
+
+app.post("/upload", upload.single("profile"), function (요청, 응답) {
+  응답.send("완료");
+});
+
+app.get("/image/:imageName", function (요청, 응답) {
+  응답.sendFile(__dirname + "/public/image/" + 요청.params.imageName);
+});
+
+app.post("/chatroom", 로그인했니, function (요청, 응답) {
+  var 저장할거 = { title: "채팅방", member: [ObjectId(요청.body.채팅수신자), 요청.user._id], date: new Date() };
+  db.collection("chatroom")
+    .insertOne(저장할거)
+    .then((결과) => {
+      응답.send("성공");
+    });
+});
+
+app.get("/chat", 로그인했니, function (요청, 응답) {
+  db.collection("chatroom")
+    .find({ member: 요청.user._id })
+    .toArray()
+    .then((결과) => {
+      응답.render("chat.ejs", { data: 결과 });
+    });
+});
+
+app.post("/message", 로그인했니, function (요청, 응답) {
+  var 저장할거 = {
+    parent: 요청.body.parent,
+    content: 요청.body.content,
+    userid: 요청.user._id,
+    date: new Date(),
+  };
+  db.collection("message")
+    .insertOne(저장할거)
+    .then(() => {
+      console.log("DB저장성공");
+      응답.send("DB저장성공");
+    });
+});
+
+app.get("/message/:id", 로그인했니, function (요청, 응답) {
+  응답.writeHead(200, {
+    Connection: "keep-alive",
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+  });
+
+  db.collection("message")
+    .find({ parent: 요청.params.id })
+    .toArray()
+    .then((결과) => {
+      응답.write("event: test\n");
+      응답.write("data: " + JSON.stringify(결과) + "\n\n");
+    });
+  const pipeline = [{ $match: { "fullDocument.parent": 요청.params.id } }];
+
+  const collection = db.collection("message");
+  const changeStream = collection.watch(pipeline);
+  changeStream.on("change", (result) => {
+    응답.write("event: test\n");
+    응답.write("data: " + JSON.stringify([result.fullDocument]) + "\n\n");
+  });
+});
+
+app.get("/socket", function (요청, 응답) {
+  응답.render("socket.ejs");
+});
+
+io.on("connection", function (socket) {
+  console.log("접속됨");
+
+  socket.on("user-send", function (data) {
+    io.emit("broadcast", data);
+  });
 });
